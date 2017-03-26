9c7a061e31f2af6d74b14f7f69c366a53a5c68bc
mac <shenghai@shenghai.local>
2017-03-26 23:15:09 +0800

    UIbutton UIimage UIimageView UIlabel 使用  和snapkit布局

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+
+target “swiftBasePro" do
+
+pod 'SnapKit', '~> 3.1.2'
+
+
+end
--- swiftBasePro/ViewController.swift
+++ swiftBasePro/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
@@ -17,7 +18,31 @@ class ViewController: UIViewController {
         var hellStr = self.sayHello("  shenghai", 20, dbResult: &dbValue);
         print(hellStr);
         hellStr = "ni hao"
+        
+        let button = UIButton.init();
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 100);
+        button.setTitle("shenghai", for: .normal);
+        button.backgroundColor = UIColor.red;
+        button.setTitleColor(UIColor.black, for: .normal);
+        self.view.addSubview(button);
+        button.addTarget(self, action: #selector(ViewController.ggwp), for: .touchUpInside);
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let imageView = UIImageView.init(frame: CGRect(x: 10, y: 10, width: 100, height: 200));
+        let image = UIImage.init(imageLiteralResourceName: "查看全景图");
+        imageView.image = image;
+        self.view.addSubview(imageView);
+        
+        let label = UILabel.init();
+        
+        label.text = "shenghai";
+        label.textColor = UIColor.green;
+        self.view.addSubview(label);
+        label.snp.makeConstraints { (make) in
+            make.left.equalTo(self.view.snp.left).offset(20);
+            make.top.equalTo(300);
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +57,10 @@ class ViewController: UIViewController {
     
     func sayHello(_ personName: String, _: Int = 8, dbResult: inout Int) -> Int {
         return 5;
+    }
+    
+    func ggwp() {
+        NSLog("slfkjksdf");
     }
 
 }
